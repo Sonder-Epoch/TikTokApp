436a79931e0c0f757b70c7795538e2fde42f5218
fate <1217645835@qq.com>
2023-01-17 15:28:44 +0800

    项目骨架

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"TikTokApp/config"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	config.Init()
+	server := gin.Default()
+	setUpRouter(server)
+	err := server.Run(config.Conf.Port)
+	if err != nil {
+		panic(err)
+	}
 }
